0fcc1f30adcac2b944beecaff48cd20a77a6d4f1
03chxng <pppingrrrong@gmail.com>
2024-10-31 23:58:32 +0800

    把舊檔案刪掉

--- coco/mainsite/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ActivityPost, CompanyPost, User
-# Register your models here.
-
-admin.site.register(ActivityPost)
-admin.site.register(CompanyPost)
-admin.site.register(User)
