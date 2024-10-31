e7ccb4d3bb6a430e81d9d55d23178567afe490b0
prkunn <87665116+prkunnn@users.noreply.github.com>
2024-10-31 20:48:34 +0800

    把舊的檔案都刪掉

---
+++ event_matcher/events/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Activity, Sponsorship
+
+admin.site.register(Activity)
+admin.site.register(Sponsorship)
+
+
+# Register your models here.
---
+++ event_matcher/events/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,get_object_or_404, redirect
+from events.models import Activity, Sponsorship
+
+def activity_list(request):
+    activities = Activity.objects.all()
+    sponsorships = Sponsorship.objects.all()
+    context = {
+        'activities': activities,
+        'sponsorships': sponsorships
+    }
+    return render(request, 'events/event_list.html', context)
+
+def sponsorship_list(request):
+    activities = Activity.objects.all()
+    sponsorships = Sponsorship.objects.all()
+    context = {
+        'activities': activities,
+        'sponsorships': sponsorships
+    }
+    return render(request, 'events/event_list.html', context)# 用於活動的收藏切換
+def toggle_activity_favorite(request, event_id):
+    event = get_object_or_404(Activity, id=event_id)
+    event.is_favorited = not event.is_favorited
+    event.save()
+    return redirect('activity_list')
+
+# 用於贊助的收藏切換
+def toggle_sponsorship_favorite(request, sponsorship_id):
+    
+    sponsorship = get_object_or_404(Sponsorship, id=sponsorship_id)
+    sponsorship.is_favorited = not sponsorship.is_favorited
+    sponsorship.save()
+    return redirect('activity_list')
+
+
+
+# Create your views here.
