5f8b9ae57db13eb82b9fe01607cd22c9217f6aa6
Carlin <carlin42605@gmail.com>
2024-11-14 12:00:03 +0800

    新增首頁叩叩特色說明

--- event_matcher/venv/Scripts/activate.bat
+++ event_matcher/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\helpdesktw\Documents\Carlin\20241114\event_matcher\venv
+set VIRTUAL_ENV=C:\Users\helpdesktw\Documents\Carlin\20241116\event_matcher\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
