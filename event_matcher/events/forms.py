398ac650eb873a901f154a6c3e2cf357a290dfe6
03chxng <pppingrrrong@gmail.com>
2024-11-26 11:35:14 +0800

    no message

--- event_matcher/events/forms.py
+++ event_matcher/events/forms.py
@@ -7,6 +7,10 @@ class UserPhotoForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['photo', 'description']
+        labels = {
+            'photo': '照片',
+            'description': '描述',  # 如果也想改 description 的标签
+        }
         
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -19,6 +23,11 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        labels = {
+            'username': '照片',
+            'email': '電子郵件',
+            'password': '密碼',
+        }
 
     def clean(self):
         cleaned_data = super().clean()
@@ -32,6 +41,10 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+        labels = {
+            'username': '照片',
+            'email': '電子郵件',
+        }
           
 class ActivityForm(forms.ModelForm):
     class Meta:
@@ -40,7 +53,21 @@ class ActivityForm(forms.ModelForm):
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
         }
+        labels = {
+            'title': '活動標題',          
+            'description': '活動描述',    
+            'location': '活動位置',    
+            'date': '活動日期',          
+            'image': '活動圖片',           
+        }
 class SponsorshipForm(forms.ModelForm):
     class Meta:
         model = Sponsorshipnew
         fields = ['title', 'description', 'amount', 'location', 'image']
+        labels = {
+            'title': '贊助標題',          
+            'description': '贊助描述',   
+            'amount': '贊助金額',        
+            'location': '贊助地點',      
+            'image': '贊助圖片',     
+        }
