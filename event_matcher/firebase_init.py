76809c2d839a4130d5aa0f0902d161f06b88ceab
Carlin <carlin42605@gmail.com>
2024-12-02 22:43:31 +0800

    改private key json

--- event_matcher/firebase_init.py
+++ event_matcher/firebase_init.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("coco-7f24e-firebase-adminsdk-xf6rb-d85cf66aba.json")
+cred = credentials.Certificate("coco-7f24e-firebase-adminsdk-xf6rb-52ca799090.json")
 firebase_admin.initialize_app(cred)
