800feaca8fb28a8c5de0b99138aa577e4c39ba8b
kcurtis618 <a0976096595@gmail.com>
2025-05-27 14:44:59 +0800

    change sqlite to MySQL

--- event_matcher/firebase_init.py
+++ event_matcher/firebase_init.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("coco-7f24e-firebase-adminsdk-xf6rb-52ca799090.json")
+cred = credentials.Certificate("coco-7f24e-firebase-adminsdk-xf6rb-430d1c6d68.json")
 firebase_admin.initialize_app(cred)
